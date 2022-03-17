bc489cc361025cc58125bbb4938ce183332f103a
Vansh Patpatia <68200019+vansh10patpatia@users.noreply.github.com>
2022-03-18 01:30:56 +0530

    basic operations

--- demo.sol
+++ demo.sol
@@ -1,4 +1,4 @@
-pragma solidity >=0.5.0 <0.6.0
+pragma solidity >=0.5.0 <0.6.0;
 
 contract DemoContract{
 
@@ -9,10 +9,23 @@ contract DemoContract{
 		uint number;
 	}
 
-	DemoStructure[] public DemoArray; //creating public array of the 
-structure 
+	DemoStructure[] public DemoArray; //creating public array of the structure 
 
-	function DemoFunction(string memory _name,uint number) public {
+	function DemoFunction(string memory _name,uint _number) public {
 		// Creating a public function
+		DemoArray.push(DemoStructure(_name,_number));
+		// Pushing a new structure into the public array
 	}
+	
+	function privateFunction(string _str) private view return(uint) {
+		//Creating a private function that can only view variables of the structure but cannot change them
+	}
+
+	function _generateRandomDna(string memory _str) private view returns (uint) {
+		//keccak256 hash function by etherum 
+        uint rand = uint(keccak256(abi.encodePacked(_str)));
+		// here we have typecast the hast into int to store it into a integer variable
+        return rand % dnaModulus;
+    }
+
 }
