bccc317d3eb3d07e6ed6f9ab1d0065a3094694c6
Vansh Patpatia <68200019+vansh10patpatia@users.noreply.github.com>
2022-03-18 00:39:36 +0530

    structure,array and function

--- demo.sol
+++ demo.sol
@@ -3,5 +3,16 @@ pragma solidity >=0.5.0 <0.6.0
 contract DemoContract{
 
 	unit demoVariable = 10 	//int variable
+		
+	struct DemoStructure{    //Declaring first structure 
+		string name;
+		uint number;
+	}
 
+	DemoStructure[] public DemoArray; //creating public array of the 
+structure 
+
+	function DemoFunction(string memory _name,uint number) public {
+		// Creating a public function
+	}
 }
